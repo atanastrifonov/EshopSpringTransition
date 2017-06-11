4716b567d59c1ec6d6431dce2cef8f013dfe89ab
Atanas Trifonov <atanas.trifonov.bg@gmail.com>
2017-06-11 20:50:49 +0300

    implementing checkout get controller method with the surcharge as an init parameter

--- src/main/java/controllers/CheckoutController.java
+++ src/main/java/controllers/CheckoutController.java
@@ -1,11 +1,28 @@
 package controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import cart.ShoppingCart;
+
 @Controller
-public class CheckoutController {
+public class CheckoutController{
+	
+	// gets it from the servlet(app) context. More convenient than implementing ServletContextAware
+	@Value("${deliverySurcharge}")
+	private String surcharge;
+	
+	@RequestMapping(value="/checkout", method=RequestMethod.GET)
+	public String checkout(HttpServletRequest request){
+		ShoppingCart cart = (ShoppingCart) request.getSession().getAttribute("cart");
+		cart.calculateTotal(surcharge);
+		
+		return "checkout";
+	}
 
 	@RequestMapping(value="/purchase", method=RequestMethod.POST)
 	public String purchase(){
@@ -13,8 +30,6 @@ public class CheckoutController {
 			return "confirmation";
 		}else{
 			return "checkout";
-		}
-		
+		}		
 	}
-	
 }
