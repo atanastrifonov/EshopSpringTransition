3035ba46fef4bb353f3e006908daafb7e31c3f17
Atanas Trifonov <atanas.trifonov.bg@gmail.com>
2018-01-01 19:59:21 +0200

    a TODO reminder for refactoring Order controllers

--- src/main/java/controllers/backoffice/EditOrderController.java
+++ src/main/java/controllers/backoffice/EditOrderController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class EditOrderController {
 	
+	// TODO: Delete this one and put everything in OrdersController
+	
 	@RequestMapping(value = "/backoffice/orders/orderId", method = GET)
 	public String viewOrder(Model model) {
 		return "admin/editOrder";
